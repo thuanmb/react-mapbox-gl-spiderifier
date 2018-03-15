b74505acf7d9e29524ac5d535c71f224e10e2cc1
Thuan Bui <bui@niometrics.com>
2018-03-15 12:36:02 +0700

    adding sample module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { ReactMapboxGlSpiderifier } from './node_modules';
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+				<ReactMapboxGlSpiderifier />
       </div>
     );
   }
