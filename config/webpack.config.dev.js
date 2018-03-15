72625146b8f9a061bced279cf2d17e41f2ed56ec
Thuan Bui <bui@niometrics.com>
2018-03-15 19:37:26 +0700

    remove the use of alias

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -89,8 +89,6 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
-
-      ComponentUtilities: path.resolve(__dirname, '../src/node_modules/common/utils'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
